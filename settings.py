4379a04ac3b78c1f17416a529b29744a9fa7fba8
Sophie-bk <sophie.k.bieker@gmail.com>
2025-12-15 20:29:30 +0100

    settings

---
+++ settings.py
@@ -0,0 +1,27 @@
+# settings.py
+from os import walk
+import pygame
+
+W, H = 600, 650 # width and height
+
+obstacle_pair_sizes = [
+    (1, 7),
+    (2, 6),
+    (3, 5),
+    (4, 4),
+    (5, 3),
+    (6, 2),
+    (7, 1)
+]
+obstacle_size = H // 10
+obstacle_gap = (obstacle_size * 2) + (obstacle_size // 2)
+ground_space = 50
+
+def import_sprite(path):
+    surface_list = []
+    for _, __, img_file in walk(path):
+        for image in img_file:
+            full_path = f"{path}/{image}"
+            img_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(img_surface)
+    return surface_list
