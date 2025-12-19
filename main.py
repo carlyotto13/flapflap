26cac1af83f6d05e8b1c5e5e7d5cfda39a8e2ad3
carlyotto13 <carlyotto13@gmail.com>
2025-12-19 08:59:06 +0100

    Tried changing the world.py and main.py code to get the hamster to jump more and be centered on the screen. I commented out the original code. The changes did not work though. :(

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
-# main
+# main.py
+#import pygame
+#import sys
+#from settings import W, H, ground_space
+#from world import World
+
+#pygame.init()
+
+#screen = pygame.display.set_mode((W, H + ground_space))
+#pygame.display.set_caption("Flap Flap")
+
+#class Main:
+    #def __init__(self, screen):
+        #self.screen = screen
+        #self.bg_img = pygame.image.load('assets/terrain/bg.png')
+        #self.bg_img = pygame.transform.scale(self.bg_img, (W, H))
+        #self.ground_img = pygame.image.load('assets/terrain/ground.png')
+        #self.ground_scroll = 0
+        #self.scroll_speed = -6
+        #self.FPS = pygame.time.Clock()
+        #self.stop_ground_scroll = False
+
+    #def main(self):
+        #world = World(screen)
+        #while True:
+            #self.stop_ground_scroll = world.game_over
+            #self.screen.blit(self.bg_img, (0, 0))
+            #for event in pygame.event.get():
+                #if event.type == pygame.QUIT:
+                    #pygame.quit()
+                    #sys.exit()
+                #elif event.type == pygame.KEYDOWN:
+                    #if not world.playing and not world.game_over:
+                        #world.playing = True
+                    #if event.key == pygame.K_SPACE:
+                        #world.update("jump")
+                    #if event.key == pygame.K_r:
+                        #world.update("restart")
+            #world.update()
+            #self.screen.blit(self.ground_img, (self.ground_scroll, H))
+            #if not self.stop_ground_scroll:
+                #self.ground_scroll += self.scroll_speed
+                #if abs(self.ground_scroll) > 35:
+                    #self.ground_scroll = 0
+            #pygame.display.update()
+            #self.FPS.tick(60)
+
+#if __name__ == "__main__":
+    #play = Main(screen)
+    #play.main()
+
+# main.py
 import pygame
 import sys
 from settings import W, H, ground_space
@@ -8,6 +59,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((W, H + ground_space))
 pygame.display.set_caption("Flap Flap")
+
 
 class Main:
     def __init__(self, screen):
@@ -25,6 +77,10 @@ class Main:
         while True:
             self.stop_ground_scroll = world.game_over
             self.screen.blit(self.bg_img, (0, 0))
+
+            jump_pressed = False
+            restart_pressed = False
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -33,10 +89,18 @@ class Main:
                     if not world.playing and not world.game_over:
                         world.playing = True
                     if event.key == pygame.K_SPACE:
-                        world.update("jump")
+                        jump_pressed = True
                     if event.key == pygame.K_r:
-                        world.update("restart")
-            world.update()
+                        restart_pressed = True
+
+            # Call update once with the appropriate action
+            if restart_pressed:
+                world.update("restart")
+            elif jump_pressed:
+                world.update("jump")
+            else:
+                world.update()
+
             self.screen.blit(self.ground_img, (self.ground_scroll, H))
             if not self.stop_ground_scroll:
                 self.ground_scroll += self.scroll_speed
@@ -44,6 +108,7 @@ class Main:
                     self.ground_scroll = 0
             pygame.display.update()
             self.FPS.tick(60)
+
 
 if __name__ == "__main__":
     play = Main(screen)
